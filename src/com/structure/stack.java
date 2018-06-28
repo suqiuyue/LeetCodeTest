7f7de85a5427b70c1a0bafa902d1de55906515f7
suqiuyue <694028008@qq.com>
2018-06-28 21:52:27 +0800

    add stack

--- src/com/structure/stack.java
+++ src/com/structure/stack.java
@@ -1,8 +1,27 @@
 package com.structure;
 
+import java.util.Stack;
+
 /**
  * Created by sqy on 2018/6/28.
  */
 public class stack {
+    public static void main(String[] args){
+        String src = "hello";
+        Stack stack = new Stack();
 
-}
+        for(int i = 0;i< src.length();i++){
+                char c = src.charAt(i);
+                stack.push(c);
+            }
+
+        for (int i = 0;i<src.length();i++){
+            if(!stack.isEmpty()){
+                System.out.println(stack.pop());
+            }
+        }
+        }
+
+    }
+
+
